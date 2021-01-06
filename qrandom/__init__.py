6b2ff197c3330e854bbdf57828c496becd14d1d8
Seto Balian <seto.balian@gmail.com>
2021-01-06 15:18:58 +0000

    Add ANU QRN fetcher

---
+++ qrandom/__init__.py
@@ -0,0 +1,1 @@
+"""ANU quantum random numbers."""
---
+++ qrandom/anu_api.py
@@ -0,0 +1,17 @@
+from typing import List
+
+import requests
+
+
+def fetch_quantum_random_numbers(length: int = 1024) -> List[int]:
+    r = requests.get(
+        "https://qrng.anu.edu.au/API/jsonI.php",
+        {"length": length, "type": "uint16"},
+    )
+    r.raise_for_status()
+    r_json = r.json()
+    if r_json["success"]:
+        return r_json["data"]
+    else:
+        raise ConnectionError("ANU API failed (with a 200 status code)")
+    return r.json()
