baf0eb10c7b4dd34919f033a7a19e10816d5a98d
Seto Balian <seto.balian@gmail.com>
2021-01-06 17:02:24 +0000

    Add mypy

--- qrandom/_anu_api.py
+++ qrandom/_anu_api.py
@@ -1,14 +1,15 @@
 """ANU API interface."""
 
-from typing import List
+from typing import Dict, List, Union
 
 import requests
 
 
 def fetch_quantum_rand_ints(length: int = 1024) -> List[int]:
+    params: Dict[str, Union[int, str]] = {"length": length, "type": "uint16"}
     r = requests.get(
         "https://qrng.anu.edu.au/API/jsonI.php",
-        {"length": length, "type": "uint16"},
+        params,
     )
     r.raise_for_status()
     r_json = r.json()
