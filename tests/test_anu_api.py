a0e4c83ca8d496a1df426ad795eae876543ad9fb
Seto Balian <seto.balian@gmail.com>
2021-01-06 16:45:34 +0000

    Update API test

--- tests/test_anu_api.py
+++ tests/test_anu_api.py
@@ -1,9 +1,9 @@
 from qrandom import _anu_api
 
 
-def test_fetch_quantum_random_numbers():
-    numbers = _anu_api.fetch_quantum_random_numbers()
+def test_fetch_quantum_rand_ints():
+    numbers = _anu_api.fetch_quantum_rand_ints()
     assert len(numbers) == 1024
     assert min(numbers) >= 0
     assert max(numbers) <= 65535
-    assert len(_anu_api.fetch_quantum_random_numbers(4)) == 4
+    assert len(_anu_api.fetch_quantum_rand_ints(4)) == 4
