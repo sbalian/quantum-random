93d16ae997ca331762ffa5c2a20dce5be316a6e1
Seto Balian <seto.balian@gmail.com>
2021-01-07 22:02:16 +0000

    Remove _quantum_random.py

--- qrandom/__init__.py
+++ qrandom/__init__.py
@@ -1,8 +1,45 @@
-"""ANU quantum random numbers."""
+"""ANU quantum random numbers.
 
-from . import _quantum_random
+Defines the QRN generator as a subclass of random.Random.
 
-_inst = _quantum_random.QuantumRandom()
+"""
+
+import random
+import warnings
+from typing import Any, NoReturn
+
+from . import _api
+
+
+class QuantumRandom(random.Random):
+    def __init__(self):
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=UserWarning)
+            super().__init__()
+        self._rand_ints = []
+
+    def _refresh_rand_ints(self, length: int = 1024) -> None:
+        self._rand_ints = _api.fetch_quantum_rand_ints(length)
+
+    def _rand_int(self) -> int:
+        if not self._rand_ints:
+            self._refresh_rand_ints(1024)
+        return self._rand_ints.pop()
+
+    def random(self) -> float:
+        return self._rand_int() / 65536
+
+    def seed(self, a=None, version=None) -> None:
+        warnings.warn("seed is ignored")
+
+    def getstate(self) -> NoReturn:
+        raise NotImplementedError
+
+    def setstate(self, state: Any) -> NoReturn:
+        raise NotImplementedError
+
+
+_inst = QuantumRandom()
 seed = _inst.seed
 random = _inst.random
 uniform = _inst.uniform
--- tests/test_qrandom.py
+++ tests/test_qrandom.py
@@ -1,4 +1,11 @@
+import pytest
 import qrandom
+
+
+def read_sample_qrns():
+    with open("tests/sample_qrns.txt") as f:
+        samples = f.read().strip().split("\n")
+    return [[int(s) for s in sample.split(",")] for sample in samples]
 
 
 def test_qrandom():
@@ -7,3 +14,39 @@ def test_qrandom():
     assert len(numbers) == 2
     assert len(set(numbers)) == len(numbers)
     assert set(numbers).issubset(set(population))
+
+
+@pytest.fixture
+def quantum_random():
+    return qrandom.QuantumRandom()
+
+
+def test_get_state(quantum_random):
+    with pytest.raises(NotImplementedError):
+        quantum_random.getstate()
+
+
+def test_set_state(quantum_random):
+    with pytest.raises(NotImplementedError):
+        quantum_random.setstate(None)
+
+
+def test_seed(quantum_random):
+    with pytest.warns(UserWarning):
+        quantum_random.seed(42, 1)
+
+
+def test_random_short(mocker, quantum_random):
+    mocker.patch(
+        "qrandom._api.fetch_quantum_rand_ints",
+        side_effect=read_sample_qrns(),
+    )
+    numbers = [
+        [quantum_random.random() for _ in range(10)],
+        [quantum_random.random() for _ in range(1200)],
+    ]
+    for numbers_ in numbers:
+        assert min(numbers_) >= 0
+        assert max(numbers_) < 1
+    assert len(numbers[0]) == 10
+    assert len(numbers[1]) == 1200
