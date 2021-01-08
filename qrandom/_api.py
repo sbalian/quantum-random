bf2f3ddc1efbb026b7873f0cf2ee1e2c9a4687fc
Seto Balian <seto.balian@gmail.com>
2021-01-08 18:35:02 +0000

    Use responses library

--- qrandom/_api.py
+++ qrandom/_api.py
@@ -4,20 +4,36 @@ from typing import Dict, List, Union
 
 import requests
 
+PARAMS: Dict[str, Union[int, str]] = {
+    "length": 1024,
+    "type": "hex16",
+    "size": 8,
+}
 
-def fetch() -> List[int]:
-    params: Dict[str, Union[int, str]] = {
-        "length": 1024,
-        "type": "hex16",
-        "size": 8,
-    }
-    r = requests.get(
-        "https://qrng.anu.edu.au/API/jsonI.php",
+URL: str = "https://qrng.anu.edu.au/API/jsonI.php"
+
+
+def make_request(
+    url: str = URL, params: Dict[str, Union[int, str]] = PARAMS
+) -> requests.models.Response:
+    response = requests.get(
+        url,
         params,
     )
-    r.raise_for_status()
-    r_json = r.json()
+    return response
+
+
+def extract_data(response: requests.models.Response) -> List[int]:
+    response.raise_for_status()
+    r_json = response.json()
     if r_json["success"]:
         return [int(number, 16) for number in r_json["data"]]
     else:
-        raise ConnectionError("ANU API failed (with a 200 status code)")
+        raise RuntimeError(
+            "ANU data extraction failed because 'success' in the response was False."  # noqa
+        )
+        # The status code is 200 when this happens
+
+
+def fetch() -> List[int]:
+    return extract_data(make_request())
--- tests/test__api.py
+++ tests/test__api.py
@@ -1,4 +1,38 @@
+import pytest
+import requests
+import responses
+import utils
 from qrandom import _api
+
+
+@responses.activate
+def test_extract_data_success():
+
+    responses.add(
+        responses.GET,
+        _api.URL,
+        json={
+            "type": "string",
+            "length": 1024,
+            "size": 8,
+            "success": True,
+            "data": utils.read_samples("hex")[0],
+        },
+        status=200,
+    )
+    response = requests.get(_api.URL)
+    numbers = _api.extract_data(response)
+    assert len(numbers) == 1024
+    assert min(numbers) >= 0
+    assert max(numbers) < 2 ** 64
+
+
+@responses.activate
+def test_extract_data_fail():
+    responses.add(responses.GET, _api.URL, json={"success": False}, status=200)
+    response = requests.get(_api.URL)
+    with pytest.raises(RuntimeError):
+        _api.extract_data(response)
 
 
 def test_fetch():
