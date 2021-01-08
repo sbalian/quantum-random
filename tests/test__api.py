5051bb6068a68072ede5b233f84031741926a3d4
Seto Balian <seto.balian@gmail.com>
2021-01-08 18:36:55 +0000

    Remove test_fetch

--- tests/test__api.py
+++ tests/test__api.py
@@ -33,10 +33,3 @@ def test_extract_data_fail():
     response = requests.get(_api.URL)
     with pytest.raises(RuntimeError):
         _api.extract_data(response)
-
-
-def test_fetch():
-    numbers = _api.fetch()
-    assert len(numbers) == 1024
-    assert min(numbers) >= 0
-    assert max(numbers) < 2 ** 64
